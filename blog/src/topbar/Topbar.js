2a3c17f5991e632902d399945899bee7d8b2f1f2
Nyna Yusof <ngnaaa@gmail.com>
2022-05-11 11:07:28 +0800

    Styled Top Bar

--- blog/src/topbar/Topbar.js
+++ blog/src/topbar/Topbar.js
@@ -5,12 +5,27 @@ const Topbar = () => {
   return (
     <div className="top">
       <div className="topLeft">
-        <i class="fa-brands fa-facebook"></i>
-        <i class="fa-brands fa-twitter"></i>
-        <i class="fa-brands fa-instagram"></i>
+        <i className="topIcon fa-brands fa-facebook"></i>
+        <i className="topIcon fa-brands fa-twitter"></i>
+        <i className="topIcon fa-brands fa-instagram"></i>
       </div>
-      <div className="topCenter">topC</div>
-      <div className="topRight">topR</div>
+      <div className="topCenter">
+        <ul className="topList">
+          <li className="topListItem">HOME</li>
+          <li className="topListItem">ABOUT</li>
+          <li className="topListItem">CONTACT</li>
+          <li className="topListItem">WRITE</li>
+          <li className="topListItem">LOGOUT</li>
+        </ul>
+      </div>
+      <div className="topRight">
+        <img
+          className="topImg"
+          src="https://media.istockphoto.com/vectors/young-muslim-female-character-wearing-a-hijab-and-casual-clothes-vector-id1206351496?k=20&m=1206351496&s=612x612&w=0&h=GV8uOBJIHA0Ow-qMPVhJKUt-QKm-dLwsVKzHr4-ynzA="
+          alt="Humaaans-Standing"
+        />
+        <i className="topSearchIcon fa-solid fa-magnifying-glass"></i>
+      </div>
     </div>
   );
 };
